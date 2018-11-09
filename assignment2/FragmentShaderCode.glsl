e8d68dbe1d3f670f5d6143081809cd7ab80d27c7
Z. Wang <wangzuowen1996@outlook.com>
2018-11-09 17:29:11 +0800

    Add files via upload
    17:28 Nov.09

--- assignment2/FragmentShaderCode.glsl
+++ assignment2/FragmentShaderCode.glsl
@@ -2,28 +2,20 @@
 
 out vec3 daColor;
 
-in vec2 UV;  //passing UV from vertex shader to fragment shader
-in vec3 normal;  //normal world
-in vec3 vertexPositionWorld;
+in vec2 UV;
 
 uniform vec4 ambientLight;
-uniform vec3 lightPositionWorld;
-uniform vec3 eyePositionWorld;
 uniform vec3 diffuseLight;
 
 uniform sampler2D texture0plane;
+uniform sampler2D texture1jeep;
+uniform sampler2D texture2block;
 
 
 void main()
 {
-	float bright = dot(normalize(lightPositionWorld - vertexPositionWorld), normalize(normal));
-	vec4 diff = vec4(bright, bright, bright, 1.0);
 
-
-
-
-	daColor = ambientLight*(0.5*texture(texture0plane, UV).rgb) + //this is ambient light
-			  diffuseLight*(0.5*texture(texture0plane, UV).rgb)*clamp(diff, 0, 1); // diffuse light
+	daColor = (texture(texture0plane, UV).rgb)  ;
 
 }
 
--- assignment2/VertexShaderCode.glsl
+++ assignment2/VertexShaderCode.glsl
@@ -1,26 +1,19 @@
 #version 430  // GLSL version your computer supports
 
 in layout(location = 0) vec3 position;
-in layout(location = 1) vec3 vertexUV;
+in layout(location = 1) vec2 vertexUV;
 in layout(location = 2) vec3 vertexNormal;
 
-
-out vec2 UV;
-out vec3 normal;
-out vec3 vertexPositionWorld;
-
-uniform mat4 MVP;
 uniform vec3 ambientLight;
 
+uniform mat4 MVP;
+
+out vec2 UV;
 
 void main()
 {
 	vec4 v = vec4(position, 1.0);
 	gl_Position = MVP*v;
-
-	normal = (MVP*vec4(vertexNormal, 0)).xyz;
-	vertexPositionWorld = (MVP*v).xyz;
-
-	theColor = ambientLight;
 	UV = vertexUV;
+
 }
