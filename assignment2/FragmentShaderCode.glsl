3d36759a20ae733e3a245640ca348a46f18fcd8b
Z. Wang <wangzuowen1996@outlook.com>
2018-11-09 22:23:52 +0800

    Add files via upload

--- assignment2/FragmentShaderCode.glsl
+++ assignment2/FragmentShaderCode.glsl
@@ -1,21 +1,35 @@
 #version 430 //GLSL version your computer supports
 
-out vec3 daColor;
+out vec4 daColor;
 
 in vec2 UV;
+in vec3 normalWorld;
+in vec3 vertexPositionWorld;
 
-uniform vec4 ambientLight;
-uniform vec3 diffuseLight;
+uniform vec3 ambientLight;
+uniform vec3 lightPositionWorld;
+uniform vec3 eyePositionWorld;
 
-uniform sampler2D texture0plane;
-uniform sampler2D texture1jeep;
-uniform sampler2D texture2block;
-
+uniform sampler2D texture0;
 
 void main()
 {
 
-	daColor = (texture(texture0plane, UV).rgb)  ;
+	vec3 lightVectorWorld = normalize(lightPositionWorld - vertexPositionWorld);
+	float brightness = dot(lightVectorWorld, normalize(normalWorld));
+	vec3 diffuseLight = vec3(brightness, brightness, brightness);
+
+
+
+	//specular light
+	//vec3 reflectedLightVectorWorld = reflect(-lightVectorWorld, normalWorld);
+	//vec3 eyeVectorWorld = normalize(eyePositionWorld - vertexPositionWorld);
+	//float s = clamp(dot(reflectedLightVectorWorld, eyeVectorWorld), 0, 1);
+	//s = pow(s, 5);
+	//vec4 specularLight = vec4(s, 0, 0, 1);
+
+	daColor = vec4((texture(texture0, UV).rgb)*(ambientLight + clamp(diffuseLight, 0, 1)), 1.0);
+	//daColor = vec4(texture(texture0, UV).rgb * ambientLight, 1.0);
 
 }
 
--- assignment2/VertexShaderCode.glsl
+++ assignment2/VertexShaderCode.glsl
@@ -1,18 +1,30 @@
 #version 430  // GLSL version your computer supports
 
-in layout(location = 0) vec3 position;
-in layout(location = 1) vec2 vertexUV;
-in layout(location = 2) vec3 vertexNormal;
+in layout(location = 0) vec3 position;  //vbo
+in layout(location = 1) vec2 vertexUV;  //uvbo
+in layout(location = 2) vec3 vertexNormal; //nvbo
 
-uniform vec3 ambientLight;
+uniform mat4 modelTransformMatrix;
+uniform mat4 projectionMatrix;
 
 uniform mat4 MVP;
 
 out vec2 UV;
+out vec3 normalWorld;
+out vec3 vertexPositionWorld;
 
 void main()
 {
 	vec4 v = vec4(position, 1.0);
+	vec4 newPosition = modelTransformMatrix * v;
+	vec4 projectedPosition = projectionMatrix * newPosition;
+
+	//calculate world coordinate of vertex and normal
+	vec4 normal_temp = modelTransformMatrix * vec4(normal, 0);
+	normalWorld = normal_temp.xyz;
+	vertexPositionWorld = newPosition.xyz;
+
+
 	gl_Position = MVP*v;
 	UV = vertexUV;
 
