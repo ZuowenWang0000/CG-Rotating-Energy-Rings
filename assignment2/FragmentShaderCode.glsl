6852c4b91a28f8778a95d54f9b699c72ed305e86
Z. Wang <wangzuowen1996@outlook.com>
2018-11-09 12:31:07 +0800

    Add files via upload

--- assignment2/FragmentShaderCode.glsl
+++ assignment2/FragmentShaderCode.glsl
@@ -1,26 +1,16 @@
 #version 430 //GLSL version your computer supports
 
 out vec4 daColor;
-in vec3 normalWorld;
-in vec3 vertexPositionWorld;
+in vec2 UV;  //passing UV from vertex shader to fragment shader
 
-uniform vec4 ambientLight;
-unifrom vec3 lightPositionWorld;
-uniform vec3 eyePositionWorld;
+
+uniform sampler2D texture0plane;
 
 
 void main()
 {
-	//Specular
-	//calculate reflect light direction
-	vec3 reflectedLightVectorWorld = reflect(-lightVectorWorld, normalWorld);
-	//calculate direction from eye to object
-	vec3 eyeVectorWorld = normalize(eyePositionWorld - vertexPositionWorld);
-	//calculate light brightness according to the angle between eye and reflect light
-	float s = clamp(dot(reflectedLightVectorWorld, eyeVectorWorld), 0 , 1);
-	//control lobe of specularLight
-	s = pow(s, 50);
-	vec4 specularLight = vec4(s, 0 , 0 , 1);
-	
-	daColor = ambientLight + clamp(diffuseLight, 0, 1) + specularLight;
+	daColor = texture(texture0plane, UV).rgb;	
+
 }
+
+
